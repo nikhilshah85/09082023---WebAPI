d941ae13740da4950fc10d980df031bdbae0ca11
Nikhil <nikhilshah.net@hotmail.com>
2023-08-09 16:21:19 +0530

    Day 1

---
+++ 09082023 - WebAPI/Day 1/greetingsAPI/greetingsAPI/Controllers/GreetingsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace greetingsAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GreetingsController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("/greetings")]
+        public IActionResult Greetings()
+        {
+            return Ok("Hello and welcome to WebAPI development world");
+        }
+    }
+}
