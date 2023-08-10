e40a4e09a37268741a37244101c620500e729ad2
Nikhil <nikhilshah.net@hotmail.com>
2023-08-10 16:21:25 +0530

    Day 2 - Inmemory collection

---
+++ 09082023 - WebAPI/Day 2/employeeManagement_API/employeeManagement_API/Controllers/EmployeeController.cs
@@ -0,0 +1,113 @@
+﻿using employeeManagement_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace employeeManagement_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        Employee eObj = new Employee(); //this is not a good practice, we should use DI instead of this
+
+        #region Http Get Methods
+        [HttpGet]
+        [Route("/employee/list")]
+        public IActionResult GetAllEmployees()
+        {
+            //we are suppose to return HttpStatus Code and HttpBody/content
+
+            var emp = eObj.GetAllEmployees();
+            return Ok(emp);
+        }
+        [HttpGet]
+        [Route("/employee/id/{eno}")]
+        public IActionResult GetEmpById(int eno)
+        {
+            try
+            {
+                var emp = eObj.GetEmpById(eno);
+                return Ok(emp);
+            }
+            catch (Exception es)
+            {
+
+                return NotFound(es.Message);
+            }
+        }
+        [HttpGet]
+        [Route("/employee/designation/{designation}")]
+        public IActionResult GetEmpByDesignation(string designation)
+        {
+            try
+            {
+                var emp = eObj.GetEmpByDesigantion(designation);
+                return Ok(emp);
+            }
+            catch (Exception es)
+            {
+                return NotFound(es.Message);
+            }
+        }
+        [HttpGet]
+        [Route("/employee/total")]
+        public IActionResult GetTotalEmployees()
+        {
+            var total = eObj.GetTotalEmployees();
+            return Ok(total);
+        }
+
+        #endregion
+
+        #region Post,Put and Delete
+        [HttpPost]
+        [Route("/employee/add")]
+        public IActionResult AddNewEmployee([FromBody] Employee newEmp)
+        {
+
+            try
+            {
+                var addResult = eObj.AddNewEmployee(newEmp);
+                return Created("", addResult);
+            }
+            catch (Exception es)
+            {
+                return BadRequest(es.Message);
+            }
+        }
+
+        [HttpDelete]
+        [Route("/employee/delete/{empno}")]
+        public IActionResult DeleteEmployee(int empno)
+        {
+            try
+            {
+                var deleteResult = eObj.DeleteEmployee(empno);
+                return Accepted(deleteResult);
+            }
+            catch (Exception es)
+            {
+
+                return NotFound(es.Message);
+            }
+        }
+
+
+        [HttpPut]
+        [Route("/employee/edit")]
+        public IActionResult UpdateEmployee([FromBody] Employee changes)
+        {
+            try
+            {
+                var updateResult = eObj.UpdateEmployee(changes);
+                return Accepted(updateResult);
+            }
+            catch (Exception es)
+            {
+
+                return NotFound(es.Message);
+            }
+        }
+        #endregion
+    }
+}
---
+++ 09082023 - WebAPI/Day 2/employeeManagement_API/employeeManagement_API/Models/Employee.cs
@@ -0,0 +1,110 @@
+﻿namespace employeeManagement_API.Models
+{
+    public class Employee
+    {
+        #region Properties
+        public int empNo { get; set; }
+        public string empName { get; set; }
+        public string empDesignation { get; set; }
+        public double empSalary { get; set; }
+        public bool empIsPermenant { get; set; }
+        #endregion
+
+        #region Data
+        static List<Employee> eList = new List<Employee>()
+        {
+            new Employee(){ empNo=101, empName="Sachin", empDesignation="Sales", empIsPermenant=true, empSalary=4000},
+            new Employee(){ empNo=102, empName="Manish", empDesignation="HR", empIsPermenant=false, empSalary=5000},
+            new Employee(){ empNo=103, empName="Mohan", empDesignation="Sales", empIsPermenant=true, empSalary=8000},
+            new Employee(){ empNo=104, empName="Rohit", empDesignation="Account", empIsPermenant=true, empSalary=2000},
+            new Employee(){ empNo=105, empName="Rohan", empDesignation="Sales", empIsPermenant=false, empSalary=3000},
+            new Employee(){ empNo=106, empName="Mike", empDesignation="Sales", empIsPermenant=true, empSalary=1200},
+            new Employee(){ empNo=107, empName="Manisha", empDesignation="HR", empIsPermenant=true, empSalary=2500}
+        };
+        #endregion           
+
+        #region Get
+        public List<Employee> GetAllEmployees()
+        {
+            return eList;
+        }
+
+        public Employee GetEmpById(int id)
+        {
+            var emp = eList.Find(em => em.empNo == id);
+            if (emp != null)
+            {
+                return emp;
+            }
+            throw new Exception("Employee Not found");
+        }
+
+        public List<Employee> GetEmpByDesigantion(string designation)
+        {
+            var emps = eList.FindAll(em => em.empDesignation == designation);
+            if (emps.Count > 0)
+            {
+                return emps;
+            }
+            else
+            {
+                throw new Exception("0 employees found working as  " + designation);
+            }
+        }
+
+        public int GetTotalEmployees()
+        {
+            return eList.Count;
+        }
+        #endregion
+
+        #region Add, update and delete
+        public string AddNewEmployee(Employee newEmpObj)
+        {
+            //we can perform validations and throw error if data is not accepted
+            if (newEmpObj.empName.Length < 3)
+            {
+                throw new Exception("Please enter a valid name of more than 3 characters");
+            }
+            eList.Add(newEmpObj);
+            return "Employee Added Successfully";
+        }
+
+        public string DeleteEmployee(int id)
+        {
+            var emp = eList.Find(em => em.empNo == id);
+            if (emp != null)
+            {
+                eList.Remove(emp);
+                return "Employee Deleted !!";
+            }
+            throw new Exception("Employee not found");
+
+        }
+        public string UpdateEmployee(Employee changes)
+        {
+            var emp = eList.Find(em => em.empNo == changes.empNo);
+            if (emp != null)
+            {
+                emp.empName = changes.empName;
+                emp.empDesignation = changes.empDesignation;
+                emp.empSalary = changes.empSalary;
+                emp.empIsPermenant = changes.empIsPermenant;
+                return "Employee Updated";
+            }
+            throw new Exception("Employee not found");
+        }
+        #endregion
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
