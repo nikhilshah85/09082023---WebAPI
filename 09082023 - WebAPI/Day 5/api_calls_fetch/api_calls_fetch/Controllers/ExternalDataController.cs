299f4787bab591a6370117151136f18ffb560cb3
Nikhil <nikhilshah.net@hotmail.com>
2023-08-16 16:29:42 +0530

    Day 5 API call fetch

---
+++ 09082023 - WebAPI/Day 5/api_calls_fetch/api_calls_fetch/Controllers/ExternalDataController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace api_calls_fetch.Controllers
+{
+    public class ExternalDataController : Controller
+    {
+
+
+        public IActionResult UserPost()
+        {
+            return View();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ 09082023 - WebAPI/Day 5/api_calls_fetch/api_calls_fetch/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace api_calls_fetch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
