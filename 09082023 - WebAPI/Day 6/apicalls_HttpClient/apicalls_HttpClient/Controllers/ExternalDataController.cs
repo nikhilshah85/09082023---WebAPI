0bc217818c6625d58409a7ff287ed8a53c5fd45b
Nikhil <nikhilshah.net@hotmail.com>
2023-08-17 16:29:36 +0530

    Day 6 - HttpClient call

---
+++ 09082023 - WebAPI/Day 6/apicalls_HttpClient/apicalls_HttpClient/Controllers/ExternalDataController.cs
@@ -0,0 +1,16 @@
+﻿using apicalls_HttpClient.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace apicalls_HttpClient.Controllers
+{
+    public class ExternalDataController : Controller
+    {
+        CommentDetails commentObj = new CommentDetails(); //we should use DI instead, we will learn about it tomorrow;
+
+        public IActionResult Comments()
+        {
+            ViewBag.userComments = commentObj.GetComments();
+            return View();
+        }
+    }
+}
---
+++ 09082023 - WebAPI/Day 6/apicalls_HttpClient/apicalls_HttpClient/Models/CommentDetails.cs
@@ -0,0 +1,41 @@
+﻿namespace apicalls_HttpClient.Models
+{
+    public class CommentDetails
+    {
+
+        public int PostId { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public string email { get; set; }
+
+        public string body { get; set; }
+
+        List<CommentDetails> cList = new List<CommentDetails>();
+        
+        public List<CommentDetails> GetComments()
+        {
+            string url = "https://jsonplaceholder.typicode.com/comments";
+
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear(); //clear all the default client setting and we will set json for call
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var call = client.GetAsync(url).Result;
+
+            if (call.IsSuccessStatusCode) //if we are getting success codes - 200 to 299 seriees, we will read data, else exception
+            {
+                var data = call.Content.ReadAsAsync<List<CommentDetails>>();
+                data.Wait();
+                cList = data.Result;
+                return cList;
+            }
+            else
+            {
+                throw new Exception("Sorry Could not get data, pleaes contact Admin");
+            }
+
+
+
+        }
+    }
+}
