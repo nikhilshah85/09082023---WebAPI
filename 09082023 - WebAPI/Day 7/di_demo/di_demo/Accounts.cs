063802c2bcc39898ecdf543c2b0f87e4f7ab56f7
Nikhil <nikhilshah.net@hotmail.com>
2023-08-18 16:06:17 +0530

    Day 7 - DI

---
+++ 09082023 - WebAPI/Day 7/di_demo/di_demo/Accounts.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+
+namespace di_demo
+{
+    public class Accounts
+    {
+        public int accNo { get; set; }
+        public string accName { get; set; }
+        public string accType { get; set; }
+        public double accBalance { get; set; }
+        public bool accIsActive { get; set; }
+
+        static List<Accounts> accList = new List<Accounts>()
+        {
+            new Accounts(){ accNo=101, accName="Nikhil", accBalance=7000, accIsActive=true, accType="Savings"},
+            new Accounts(){ accNo=102, accName="Rahul", accBalance=7000, accIsActive=true, accType="Savings"},
+            new Accounts(){ accNo=103, accName="Suman", accBalance=7000, accIsActive=true, accType="Current"},
+            new Accounts(){ accNo=104, accName="Preeti", accBalance=7000, accIsActive=false, accType="Savings"},
+            new Accounts(){ accNo=105, accName="Mohan", accBalance=7000, accIsActive=true, accType="Savings"},
+            new Accounts(){ accNo=106, accName="Rohan", accBalance=7000, accIsActive=false, accType="Current"},
+
+        };
+
+        public List<Accounts> GetAllAccounts()
+        {
+            return accList;
+        }
+
+
+
+
+
+
+    }
+}
---
+++ 09082023 - WebAPI/Day 7/di_demo/di_demo/Controllers/AccountsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace di_demo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+
+        // Accounts accObj = new Accounts(); //this is bad, please use DI instead
+
+
+        Accounts _accObj; //not createing the object
+
+
+        public AccountsController(Accounts accObjREF)
+        {
+            _accObj = accObjREF;
+        }
+
+        [HttpGet]
+        [Route("/accounts/list")]
+        public IActionResult AccountsList()
+        {
+            var data = _accObj.GetAllAccounts();
+            return Ok(data);
+        }
+    }
+}
